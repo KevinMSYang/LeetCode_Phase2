335e228c22263e051792a3be7e0c366674bb2119
KevinMSYang <beerminshen@gmail.com>
2024-08-17 22:24:04 -0700

    08/17

--- src/BinaryTree/FindDuplicateSubtrees.java
+++ src/BinaryTree/FindDuplicateSubtrees.java
@@ -25,8 +25,30 @@
 
 package BinaryTree;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.swing.tree.TreeNode;
+
 public class FindDuplicateSubtrees {
     public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
-        
+    	List<TreeNode> ans = new ArrayList<>();
+    	Map<String, Integer> map = new HashMap<>();
+    	
+    	helper(root, ans, map);
+    	return ans;
+    }
+    private String helper(TreeNode node, List<TreeNode> ans, Map<String, Integer> map) {
+    	if (node == null) return "";
+    	String left = helper(node.left, ans, map);
+    	String right = helper(node.right, ans, map);
+    	String str = node.val + "#" + left + "#" + right;
+    	
+    	if (map.merge(str, 1, Integer::sum) == 2) {
+    		ans.add(node);
+    	}
+    	return str;
     }
 }
